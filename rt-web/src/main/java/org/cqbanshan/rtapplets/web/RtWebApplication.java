f398c129e3cef97a1b78d5b114ea2ed073768a5d
李绪兴 <lixx@cqbanshan.com>
2021-07-13 21:47:03 +0800

    添加模块之间的引用

---
+++ rt-web/src/main/java/org/cqbanshan/rtapplets/web/RtWebApplication.java
@@ -0,0 +1,13 @@
+package org.cqbanshan.rtapplets.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtWebApplication.class, args);
+    }
+
+}
